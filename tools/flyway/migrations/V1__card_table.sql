bf26b8acc278917f271d0f523b7ed9845a718c56
Nikhil Ghosh <nghosh606@gmail.com>
2025-09-06 10:38:02 -0400

    Imported flyway

---
+++ tools/flyway/migrations/V1__card_table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE cards (
+    card_id SERIAL NOT NULL PRIMARY KEY,
+	english_name CHAR(192) NOT NULL,
+	released_at DATE NOT NULL,
+	mana_cost CHAR(48),
+	typeline CHAR(192),
+	power SMALLINT,
+	toughness SMALLINT,
+	loyalty SMALLINT,
+	oracle_text VARCHAR,
+	set_code CHAR,
+	art_url VARCHAR
+);
